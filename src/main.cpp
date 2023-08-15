066604aaca93e0fe0d6e0e966ab7308cf5585bde
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-08-15 00:09:19 -0300

    Teste inicial da ligações eletricas

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int IN1 = 26;
+int IN2 = 27;
+int velocidadeA = 25;
+
+int IN3 = 14;
+int IN4 = 12;
+int velocidadeB = 13;
+
+int velocidade = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(velocidadeA, OUTPUT);
+    pinMode(velocidadeB, OUTPUT);
+
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    while (velocidade < 100) {
+        analogWrite(velocidadeA, velocidade);
+        analogWrite(velocidadeB, velocidade);
+        velocidade = velocidade + 2;
+        delay(200);
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    while (velocidade > 50) {
+        analogWrite(velocidadeA, velocidade);
+        analogWrite(velocidadeB, velocidade);
+        velocidade = velocidade - 2;
+        delay(200);
+    }
 }
